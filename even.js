b0885318a2e4b02eec7b2042eb45c0706a1a8738
soumyashree sarangi <soumyashree.sarangi@gmail.com>
2019-03-13 02:18:40 +0000

    even.js exercise with 'isEven' function completed

---
+++ even.js
@@ -0,0 +1,13 @@
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+/*
+var tenIsEven = isEven(10);
+var elevenIsEven = isEven(11);
+
+console.log("Ten is Even " + tenIsEven);
+console.log("Eleven is Even " + elevenIsEven); */
+
+console.log("Ten is Even " + isEven(10));
+console.log("Eleven is Even " + isEven(11));
